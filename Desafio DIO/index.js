5322e2b7d76d145e6b66f7b545acd1635beb1853
Fudoshim <fudoshim@proton.me>
2025-03-19 18:56:26 -0400

    teste 01

---
+++ Desafio DIO/index.js
@@ -0,0 +1,31 @@
+// Definindo o nome e a quantidade de XP do herói
+let nome = "Fudoshim";
+let xp = 8000;
+
+// Definindo a variável que armazenará o nível
+let nivel = "";
+
+// Estrutura de decisão para determinar o nível baseado no XP
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Exibindo a mensagem final
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+
+
+
